e4f380c48ec2fd0157bdabce3ed50d5f6d729c2a
Darin Egan <darinegan@gmail.com>
2018-09-25 10:54:02 +0100

    Invoke jq with file arg as relative path

--- utils/Export-TFVars.ps1
+++ utils/Export-TFVars.ps1
@@ -3,6 +3,11 @@ Param (
     [string] $Path
 )
 
-$env:ARM_CLIENT_ID = $(jq -r .appId $Path)
-$env:ARM_CLIENT_SECRET = $(jq -r .password $Path)
-$env:ARM_TENANT_ID = $(jq -r .tenant $Path)
+$ParentPath = Split-Path -Path $Path -Parent
+$LeafPath = Split-Path -Path $Path -Leaf
+
+Push-Location $ParentPath
+    $env:ARM_CLIENT_ID = $(jq -r .appId $LeafPath)
+    $env:ARM_CLIENT_SECRET = $(jq -r .password $LeafPath)
+    $env:ARM_TENANT_ID = $(jq -r .tenant $LeafPath)
+Pop-Location
